407301b50efa4ae491ac96bd44c91feeaf88b17c
LamVH <lamvhhe186943@fpt.edu.vn>
2025-10-14 19:28:08 +0700

    update giao dien shopdeetails

--- src/main/java/fpt/swp/springmvctt/itp/controller/HomeController.java
+++ src/main/java/fpt/swp/springmvctt/itp/controller/HomeController.java
@@ -1,4 +1,13 @@
 package fpt.swp.springmvctt.itp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping({"/"})
+    public String index() {
+        return "index"; // trỏ tới templates/index.html
+    }
 }
