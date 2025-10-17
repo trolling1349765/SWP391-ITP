92b3502a94e8a6b767a9d4d3f7d240f77b52df5c
TuTM <trolling1349765@gmail.com>
2025-10-17 12:53:51 +0700

    v6 restriction      filter      search      paging

--- src/main/java/fpt/swp/springmvctt/itp/controller/UserRestrictionController.java
+++ src/main/java/fpt/swp/springmvctt/itp/controller/UserRestrictionController.java
@@ -4,15 +4,12 @@ import fpt.swp.springmvctt.itp.entity.UserRestriction;
 import fpt.swp.springmvctt.itp.service.UserRestrictionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin/user-restriction")
--- src/main/java/fpt/swp/springmvctt/itp/service/impl/UserRestrictionServiceImpl.java
+++ src/main/java/fpt/swp/springmvctt/itp/service/impl/UserRestrictionServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
