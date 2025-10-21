9ad3d506d568f06ae8f91beb2c3a038d275d7141
LamVH <lamvhhe186943@fpt.edu.vn>
2025-10-21 11:40:26 +0700

    check na

--- src/main/java/fpt/swp/springmvctt/itp/dto/request/StockForm.java
+++ src/main/java/fpt/swp/springmvctt/itp/dto/request/StockForm.java
@@ -1,6 +1,7 @@
 package fpt.swp.springmvctt.itp.dto.request;
 
 import lombok.Data;
+import java.math.BigDecimal;
 
 @Data
 public class StockForm {
@@ -8,4 +9,7 @@ public class StockForm {
     private String serial;
     private String code;
     private Integer quantity;
+    private BigDecimal faceValue;
+    private String infomation;
+    private String status;
 }
