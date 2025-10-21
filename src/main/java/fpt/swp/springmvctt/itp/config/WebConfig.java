b6fcbfeb4dbbafb0a007f6103d3e9954891257de
LamVH <lamvhhe186943@fpt.edu.vn>
2025-10-22 00:19:15 +0700

    fix shop

--- src/main/java/fpt/swp/springmvctt/itp/config/WebConfig.java
+++ src/main/java/fpt/swp/springmvctt/itp/config/WebConfig.java
@@ -1,22 +1,15 @@
 package fpt.swp.springmvctt.itp.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.nio.file.Path;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${app.upload-dir:uploads/assets/img}")
-    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //uploaded images
-        String abs = Path.of(uploadDir).toAbsolutePath().normalize().toString().replace("\\","/");
         registry.addResourceHandler("/assets/img/**")
-                .addResourceLocations("file:" + abs + "/")
                 .addResourceLocations("classpath:/assets/img/");
     }
 }
