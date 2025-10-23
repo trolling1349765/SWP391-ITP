e72a94c08b58634dcddd34baa571394840c0ef8a
LamVH <lamvhhe186943@fpt.edu.vn>
2025-10-23 11:30:14 +0700

    webfig

--- src/main/java/fpt/swp/springmvctt/itp/config/WebConfig.java
+++ src/main/java/fpt/swp/springmvctt/itp/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/img/**")
                 .addResourceLocations("classpath:/assets/img/");
-        
+
         registry.addResourceHandler("/assets/ajax/**")
                 .addResourceLocations("classpath:/assets/ajax/");
     }
